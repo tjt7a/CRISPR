b1ef722b83d3cb916bf9c802148abbf4b5b59fb8
Tommy Tracy II <tjt7a@virginia.edu>
2022-11-18 11:18:05 -0500

    Updated experiment to work with 8 cpus

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,27 +1,50 @@
-cd genomes
+#!/bin/bash
+
+cd genomes/forward
 echo "Unzipping forward human genome gzip file"
-gunzip -k forward_hg38_no_n.txt.gz
 
-cd ..
+# If unzipped file doesnt exist, unzip
+if [ ! -f forward_hg38_no_n.txt ]
+then
+	gunzip -k forward_hg38_no_n.txt.gz
+fi
 
-echo "Running hyperscan with sgRNA + PAM against forward HG38 DNA Strand"
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward_hg38_no_n.txt > results/forward_results.txt
-tail -n 3 results/forward_results.txt | grep "Simulation Time"
-echo ""
+echo "Splitting forward human genome into 4 chunks"
+split -n 4 forward_hg38_no_n.txt
 
-cd genomes
-rm forward_hg38_no_n.txt
-echo "Unzipping reverse compliment human genome gzip file"
-gunzip -k reverse_compliment_hg38_no_n.txt.gz
+cd ../rc
+echo "Unzipping reverse complement human genome gzip file"
 
-cd ..
+# If unzipped file doesnt exist, unzip
+if [ ! -f reverse_compliment_hg38_no_n.txt ]
+then
+	gunzip -k reverse_compliment_hg38_no_n.txt.gz
+fi
+	
+echo "Splitting reverse complement human genome into 4 chunks"
+split -n 4 reverse_compliment_hg38_no_n.txt
 
-echo "Running hyperscan with sgRNA + PAM against reverse compliment HG38 DNA Strand"
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/reverse_compliment_hg38_no_n.txt > results/reverse_compliment_results.txt
-tail -n 3 results/reverse_compliment_results.txt | grep "Simulation Time"
-echo ""
+cd ../../
+echo "Running hyperscan with sgRNA + PAM against forward and backward HG38 DNA Strand"
 
-cd genomes
-rm reverse_compliment_hg38_no_n.txt
+start=`date +%s`
 
+# Forward
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xaa > results/forward_results_xaa.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xab > results/forward_results_xab.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xac > results/forward_results_xac.txt & 
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xad > results/forward_results_xad.txt &
+
+# Backward
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xaa > results/rc_results_xaa.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xab > results/rc_results_xab.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xac > results/rc_results_xac.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xad > results/rc_results_xad.txt &
+
+wait
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "It took $runtime seconds to run all alignments with 8 cores!"
 echo "Done with experiment!"
