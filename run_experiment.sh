543e7e6cf1404d852f0d1152383d13a37e027527
Tommy Tracy II <tjt7a@virginia.edu>
2022-12-08 12:51:36 -0500

    Removed splitting input

--- run_experiment.sh
+++ run_experiment.sh
@@ -9,9 +9,6 @@ then
 	gunzip -k forward_hg38_no_n.txt.gz
 fi
 
-echo "Splitting forward human genome into 4 chunks"
-split -n 4 forward_hg38_no_n.txt
-
 cd ../rc
 echo "Unzipping reverse complement human genome gzip file"
 
@@ -21,30 +18,22 @@ then
 	gunzip -k reverse_compliment_hg38_no_n.txt.gz
 fi
 	
-echo "Splitting reverse complement human genome into 4 chunks"
-split -n 4 reverse_compliment_hg38_no_n.txt
-
 cd ../../
 echo "Running hyperscan with sgRNA + PAM against forward and backward HG38 DNA Strand"
 
 start=`date +%s`
 
 # Forward
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xaa > results/forward_results_xaa.txt &
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xab > results/forward_results_xab.txt &
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xac > results/forward_results_xac.txt & 
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/xad > results/forward_results_xad.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/forward/forward_hg38_no_n.txt > results/forward_results.txt &
 
 # Backward
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xaa > results/rc_results_xaa.txt &
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xab > results/rc_results_xab.txt &
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xac > results/rc_results_xac.txt &
-./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/xad > results/rc_results_xad.txt &
+./hsrun dbs/lev_1_4_sgRNA_with_PAM.hs genomes/rc/reverse_compliment_hg38_no_n.txt > results/rc_results.txt &
+
 
 wait
 
 end=`date +%s`
 runtime=$((end-start))
 
-echo "It took $runtime seconds to run all alignments with 8 cores!"
+echo "It took $runtime seconds to run all alignments with 2 cores!"
 echo "Done with experiment!"
