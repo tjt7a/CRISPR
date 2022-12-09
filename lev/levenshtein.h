5fc8f93e6d8593f6eb0bf0fc8ffef768b1567ebf
Tommy Tracy II <tjt7a@virginia.edu>
2022-12-09 15:40:25 -0500

    Added Levenshtein generator code

---
+++ lev/levenshtein.h
@@ -0,0 +1,12 @@
+#ifndef LEV_H
+#define LEV_H
+#include "automata.h"
+#include <string>
+
+void genLevenshtein(Automata *a,
+                    uint32_t pattern_id,
+                    std::string pattern,
+                    uint32_t edit_distance,
+                    bool restricted,
+		    bool pam);
+#endif
